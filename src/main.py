b33c6c8ce61a9bb48ae290ba2c6e043f0e23a2ab
ahmad <ahmed.salamony497@gmail.com>
2022-08-14 11:46:03 +0200

    Refactor code to build a pipeline

--- src/main.py
+++ src/main.py
@@ -1,69 +1,96 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
-from utils import DataCleaner
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+from utils import DataExtractor, split_train_dev_x_y
 
 
 class HeadPoseEstimator:
     def __init__(self):
-        self.pitch_model = RandomForestRegressor(n_estimators=100, max_depth=8, min_samples_leaf=3)
-        self.yaw_model = RandomForestRegressor(n_estimators=100, max_depth=8, min_samples_leaf=3)
-        self.roll_model = RandomForestRegressor(n_estimators=100, max_depth=8, min_samples_leaf=3)
+        self.pca = PCA(n_components=0.98)
+        self.scaler = StandardScaler()
+        self.pitch_model = RandomForestRegressor(
+            n_estimators=100, max_depth=8, min_samples_leaf=3)
+        self.yaw_model = RandomForestRegressor(
+            n_estimators=100, max_depth=8, min_samples_leaf=3)
+        self.roll_model = RandomForestRegressor(
+            n_estimators=100, max_depth=8, min_samples_leaf=3)
         # self.pitch_model = SVR(C=10)
         # self.yaw_model = SVR(C=500)
         # self.roll_model = SVR(C=10)
 
+
+    def _normalize_features(self, x):
+        # Normalization Step:
+        # consider the nose point as the center to avoid the change
+        # of faces position in images. (simply subtract all points from the nose point)
+        X = x.copy()
+        idx = np.arange(0, X.shape[1], 1)
+        X[:, idx % 2 == 0] -= X[:, 0].reshape(-1, 1)
+        X[:, idx % 2 == 1] -= X[:, 1].reshape(-1, 1)
+        # devide by the distance_from_nose_to_chin to normalize the distance from the face to the camera.
+        chin_point_idx = 377
+        chin_x = (chin_point_idx + 1) * 2
+        chin_y = chin_x + 1
+        d = np.sqrt(chin_x**2 + chin_y**2)
+        X = X / d
+        return X
+
     def fit(self, x, y):
-        self.pitch_model = self.pitch_model.fit(x, y[:, 0])
-        self.yaw_model = self.yaw_model.fit(x, y[:, 1])
-        self.roll_model = self.roll_model.fit(x, y[:, 2])
+        X = self._normalize_features(x)
+        X = self.pca.fit_transform(X)
+        X = self.scaler.fit_transform(X)
+
+        pd.DataFrame(data=X, columns=[
+                     f'x{i}' for i in range(X.shape[1])]).to_csv('X.csv')
+
+        self.pitch_model = self.pitch_model.fit(X, y[:, 0])
+        self.yaw_model = self.yaw_model.fit(X, y[:, 1])
+        self.roll_model = self.roll_model.fit(X, y[:, 2])
         return self
 
     def predict(self, x):
+        X = self._normalize_features(x)
+        X = self.pca.transform(X)
+        X = self.scaler.transform(X)
+
         return [
-            self.pitch_model.predict(x),
-            self.yaw_model.predict(x),
-            self.roll_model.predict(x)
-            ]
+            self.pitch_model.predict(X),
+            self.yaw_model.predict(X),
+            self.roll_model.predict(X)
+        ]
 
     def report(self, y, h):
-        print('Pitch r2_score =', r2_score(y[:, 0] , h[0]))
-        print('Yaw r2_score =', r2_score(y[:, 1] , h[1]))
-        print('Roll r2_score =', r2_score(y[:, 2] , h[2]))
+        print('Pitch r2_score =', r2_score(y[:, 0], h[0]))
+        print('Yaw r2_score =', r2_score(y[:, 1], h[1]))
+        print('Roll r2_score =', r2_score(y[:, 2], h[2]))
 
 
 if __name__ == '__main__':
     np.random.seed(42)
-    df_path = 'cleaned_data_mp.csv'
-    # df_path = 'cleaned_data_21pts.csv'
+    df_path = 'data.csv'
 
     try:
         df = pd.read_csv(df_path, index_col=0)
+        print('df exists')
     except:
-        cleaner = DataCleaner(
+        extractor = DataExtractor(
             '/home/ahmad/projects/python/head_pose_estimation/AFLW2000-3D/AFLW2000')
-        df = cleaner.get_cleaned_df()
+        df = extractor.get_data_df()
         df.to_csv(df_path)
 
-    # print(df.head())
-    # print(df.describe())
-
-    def split_x_y(data):
-        return data[:, :-3], data[:, -3:]
-
-    data_train, data_test = train_test_split(df, train_size=0.90)
-    # data_dev, data_test = train_test_split(data_dev_test, train_size=0.50, shuffle=False)
-    
-    X_train, Y_train = split_x_y(data_train.values)
-    # X_dev, Y_dev = split_x_y(data_dev.values)
-    X_test, Y_test = split_x_y(data_test.values)
+    X_train, Y_train, X_dev, Y_dev = split_train_dev_x_y(df)
 
     model = HeadPoseEstimator()
     model = model.fit(X_train, Y_train)
-    # validate results on validation set
-    h = model.predict(X_test)
+    # Measure errors on train set
+    y_hat = model.predict(X_train)
+    model.report(Y_train, y_hat)
 
-    model.report(Y_test, h)
+    # validate results on validation set
+    h = model.predict(X_dev)
+    model.report(Y_dev, h)
--- src/utils.py
+++ src/utils.py
@@ -6,156 +6,140 @@ import mediapipe as mp
 from glob import glob
 from pathlib import Path
 import pandas as pd
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
 
 def split_x_y(data):
+    ''' 
+        Takes a matrix which its last 3 columns are the labels,
+        Returns a tuple of 2 matrices (features and labels).
+    '''
     return data[:, :-3], data[:, -3:]
 
+
 def split_train_dev_x_y(df, train_size=0.9):
+    '''
+        Takes a data frame as input,
+        split the data into train, dev sets,
+        then, split each set into (features, labels) matrices.
+    '''
     data_train, data_test = train_test_split(df, train_size=train_size)
     X_train, Y_train = split_x_y(data_train.values)
     X_dev, Y_dev = split_x_y(data_test.values)
-    return X_train, Y_train, X_dev, Y_dev 
+    return X_train, Y_train, X_dev, Y_dev
 
-def draw_axis(img, yaw, pitch, roll, tdx, tdy, size = 100):
+
+def draw_axis(img, yaw, pitch, roll, tdx=None, tdy=None, size=100):
     yaw = -yaw
-    
-    if tdx != None and tdy != None:
-        tdx = tdx
-        tdy = tdy
-    else:
+
+    if tdx == None or tdy == None:
         height, width = img.shape[:2]
         tdx = width / 2
         tdy = height / 2
 
     # X-Axis pointing to right drawn in red
     x1 = size * (np.cos(yaw) * np.cos(roll)) + tdx
-    y1 = size * (np.cos(pitch) * np.sin(roll) + np.cos(roll) * np.sin(pitch) * np.sin(yaw)) + tdy
+    y1 = size * (np.cos(pitch) * np.sin(roll) + np.cos(roll)
+                 * np.sin(pitch) * np.sin(yaw)) + tdy
 
     # Y-Axis | drawn in green
     x2 = size * (-np.cos(yaw) * np.sin(roll)) + tdx
-    y2 = size * (np.cos(pitch) * np.cos(roll) - np.sin(pitch) * np.sin(yaw) * np.sin(roll)) + tdy
+    y2 = size * (np.cos(pitch) * np.cos(roll) - np.sin(pitch)
+                 * np.sin(yaw) * np.sin(roll)) + tdy
 
     # Z-Axis (out of the screen) drawn in blue
     x3 = size * (np.sin(yaw)) + tdx
     y3 = size * (-np.cos(yaw) * np.sin(pitch)) + tdy
 
-    cv2.line(img, (int(tdx), int(tdy)), (int(x1),int(y1)),(0,0,255),3)
-    cv2.line(img, (int(tdx), int(tdy)), (int(x2),int(y2)),(0,255,0),3)
-    cv2.line(img, (int(tdx), int(tdy)), (int(x3),int(y3)),(255,0,0),2)
+    cv2.line(img, (int(tdx), int(tdy)), (int(x1), int(y1)), (0, 0, 255), 3)
+    cv2.line(img, (int(tdx), int(tdy)), (int(x2), int(y2)), (0, 255, 0), 3)
+    cv2.line(img, (int(tdx), int(tdy)), (int(x3), int(y3)), (255, 0, 0), 2)
 
     return img
 
 
-class DataCleaner:
+class DataExtractor:
     def __init__(self, path):
         self.data_dir = Path(path)
-        self.pca = PCA(n_components=8)
-        self.scaler = MinMaxScaler()
         mp_face_mesh = mp.solutions.face_mesh
         self.face_mesh = mp_face_mesh.FaceMesh(
             static_image_mode=True,
-            max_num_faces=2
-            )
+            max_num_faces=2,
+        )
 
 
-    def get_features(self, marks):
-        # Normalization Step:
-        # devide by the nose point to normalize the distance from face to the camera.
-        # consider the nose point as the center to avoid the change
-        # of faces position in images.
-        # print('marks =', marks)
-        nose_point = marks[1]
-        marks = marks - nose_point
-        # print('marks after centering =', marks)
-        d = np.sqrt(np.sum((marks[377] - marks[1])**2)) # distance from nose to chin
-        # print('d =', d)
-        marks = marks / d
-        features = marks.flatten(order='F')
-        return features
-
-    # def _get_cleaned_features(self, features)
     def _get_labels(self, mat):
         pose = mat.get('Pose_Para')[0, :3]
-        labels = pose # * 180.0 / pi
-        return labels
+        return pose.reshape(1, -1)
 
-    def get_cleaned_df(self):
+
+    def extract_features_from_img(self, image):
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Get the result
+        results = self.face_mesh.process(img)
+        img_h, img_w, img_c = img.shape
+        # if number of detected faces is 1 (to execlude the data with more than 1 face but only one label)
+        faces_features = []
+        if results.multi_face_landmarks:
+            for face_landmarks  in results.multi_face_landmarks:
+                # Get the 468 points.
+                features = np.array([
+                    [
+                        int(lm.x * img_w),
+                        int(lm.y * img_h)
+                    ]
+                    for lm in face_landmarks.landmark
+                ], dtype=np.float64)
+
+                features = features.flatten().reshape(1, -1)
+                faces_features.append(features)
+        return faces_features
+
+
+    def get_data(self):
         all_imgs = glob(str(self.data_dir / '*.jpg'))
         data = []
-        indices = []
-        # print('len(all_imgs) =', len(all_imgs))
         for img in all_imgs:
             img_name = Path(img).stem
             img = cv2.imread(img)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            # Get the result
-            results = self.face_mesh.process(img)
-            # Convert the color space from RGB to BGR
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            img_h, img_w, img_c = img.shape
-            
-            if results.multi_face_landmarks and len(results.multi_face_landmarks) == 1:
-                # print('results.multi_face_landmarks =', len(results.multi_face_landmarks))
-                for face_landmarks in results.multi_face_landmarks:
-                    indices.append(img_name)
-                    # print(face_landmarks.landmark[0])
-                    features = np.array([
-                        [
-                            int(lm.x * img_w),
-                            int(lm.y * img_h)
-                        ]
-                        for lm in face_landmarks.landmark 
-                    ], dtype=np.float64)
+            faces = self.extract_features_from_img(img)
+            if faces and len(faces) == 1:
+                features = faces[0]    
+                mat = sio.loadmat(str(self.data_dir / f'{img_name}.mat'))
+                labels = self._get_labels(mat)
+                row = np.concatenate((features, labels), axis=1)
+                # print('row.shape =', row.shape)
+                data.append((img_name, row))
+        return data
+        
 
-                    features = self.get_features(features)
+    def get_data_df(self, data):
+        n = (data[0][1].shape[1] - 3) // 2
 
-                    mat = sio.loadmat(str(self.data_dir / f'{img_name}.mat'))
-                    labels = self._get_labels(mat)
-                    data.append((features, labels))
-        #         cv2.imshow(img_name, img)
-        # cv2.waitKey(0)
-        features = np.array([d[0] for d in data])
-        # print('features shape =', features.shape)
-        features = self.apply_pca(features)
-        features = self.scaler.fit_transform(features)
-
-        data = np.concatenate((features, [d[1] for d in data]), axis=1)
-        # print(data.shape)
-        n = data.shape[1]-3
-        cols = [f'x{i}' for i in range(1, n+1)] \
-             + ['pitch', 'yaw', 'roll']
+        x_cols_names = [f'x{i}' for i in range(n)]
+        y_cols_names = [f'y{i}' for i in range(n)]
+        
+        features_cols_names = []
+        for i in range(len(x_cols_names)):
+            features_cols_names.append(x_cols_names[i])
+            features_cols_names.append(y_cols_names[i])
+        
+        labels_cols_names = ['pitch', 'yaw', 'roll']
+        cols = features_cols_names + labels_cols_names
 
         result = pd.DataFrame(
-            index=indices,
-            data=data,
+            index=[d[0] for d in data],
+            data=np.squeeze([d[1] for d in data]),
             columns=cols,
         ).sort_index()
 
-        # drop the zero columns after centering around them.
-        # cols = [c for c in result.columns if len(result[c].unique()) == 1]
         return result
-
-
-    def apply_pca(self, data):
-        new_data = self.pca.fit_transform(data)
-        print('variance ratio =', self.pca.explained_variance_ratio_.sum())
-        return new_data
-
-    def transform(self, data):
-        result = self.pca.transform(data)
-        result = self.scaler.transform(result)
-        return result    
-
-
+        
 
 if __name__ == '__main__':
+    path = '/home/ahmad/projects/python/head_pose_estimation/AFLW2000-3D/AFLW2000'
+    extractor = DataExtractor(path)
+
     # Init a cleaner object.
-    cleaner = DataCleaner(
-        '/home/ahmad/projects/python/head_pose_estimation/AFLW2000-3D/AFLW2000')
-    
-    df = cleaner.get_cleaned_df()
-    df.to_csv('cleaned_data_mp.csv')
+    df = extractor.get_data_df()
+    df.to_csv('data.csv')
